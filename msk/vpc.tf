b21e5d0fe9d7c8b86334462cf4f58bd9639d7e0c
fabio.rodriguez@zup.com.br <fabio.rodriguez@zup.com.br>
2021-03-17 16:44:53 -0300

    add terraform files

---
+++ msk/vpc.tf
@@ -0,0 +1,25 @@
+resource "aws_vpc" "vpc-msk-mwaa" {
+  cidr_block = "192.168.0.0/22"
+}
+
+data "aws_availability_zones" "azs" {
+  state = "available"
+}
+
+resource "aws_subnet" "subnet_az1" {
+  availability_zone = data.aws_availability_zones.azs.names[0]
+  cidr_block        = "192.168.0.0/24"
+  vpc_id            = aws_vpc.vpc.id
+}
+
+resource "aws_subnet" "subnet_az2" {
+  availability_zone = data.aws_availability_zones.azs.names[1]
+  cidr_block        = "192.168.1.0/24"
+  vpc_id            = aws_vpc.vpc.id
+}
+
+resource "aws_subnet" "subnet_az3" {
+  availability_zone = data.aws_availability_zones.azs.names[2]
+  cidr_block        = "192.168.2.0/24"
+  vpc_id            = aws_vpc.vpc.id
+}
